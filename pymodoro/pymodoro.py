62566dfb6fcb8fb5bb131b3c967fa8abba7219cb
adam cushner <sweettuse@gmail.com>
2022-11-12 03:42:10 -0500

    separate logic of CountdownTimer from textual

--- pymodoro/pymodoro.py
+++ pymodoro/pymodoro.py
@@ -16,10 +16,10 @@ from textual.containers import Horizontal
 from textual.binding import Binding
 from pymodoro.widgets.text_input import TextInput
 
-from widgets.timer import CountdownTimer
+from widgets.timer import CountdownTimer, CountdownTimerWidget
 
 
-class CountdownTimerWidget(Static, can_focus=True):
+class CountdownTimerContainer(Static, can_focus=True):
     BINDINGS = [
         Binding("d", "dump_state", "dump state"),
     ]
@@ -29,14 +29,14 @@ class CountdownTimerWidget(Static, can_focus=True):
             TextInput(id="linear", placeholder="linear issue id..."),
             TextInput(id="description", placeholder="description"),
             Button("start", id="start", variant="success"),
-            CountdownTimer(60),
+            CountdownTimerWidget(countdown_timer=CountdownTimer(60)),
             Button("stop", id="stop", variant="error"),
         )
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
         """Event handler called when a button is pressed."""
         button_id = event.button.id
-        ct = self.query_one(CountdownTimer)
+        ct = self.query_one(CountdownTimerWidget)
         if button_id == "start":
             ct.start()
             self.add_class("started")
@@ -45,32 +45,28 @@ class CountdownTimerWidget(Static, can_focus=True):
             ct.stop()
 
     def action_dump_state(self) -> dict[str, Any]:
-        # for w in self.query(Input):
-        # w.
-        self.log("================================")
-        self.log("================================")
-        self.log("================================")
-        self.log("================================")
-        states = [ti.dump_state() for ti in self.query(TextInput)]
-        states.append(self.query_one(CountdownTimer).dump_state())
-        self.log(list(self.query(TextInput)))
-        self.log(list(self.query(CountdownTimer)))
-        self.log(type(self.query_one("#linear")))
-        # pickle.dumps(self.query_one('#linear'))
-        self.log("================================")
-        self.log("================================")
-        self.log("================================")
-        self.log("================================")
+        self.log("dump state currently does nothing")
+        pass
+        # self.log("================================")
+        # self.log("================================")
+        # self.log("================================")
+        # self.log("================================")
+        # # states = [ti.dump_state() for ti in self.query(TextInput)]
+        # # states.append(self.query_one(CountdownTimerWidget).dump_state())
+        # # self.log(states)
+        # self.log("================================")
+        # self.log("================================")
+        # self.log("================================")
+        # self.log("================================")
 
 
 class Pymodoro(App):
     CSS_PATH = "css/pymodoro.css"
 
     def compose(self) -> ComposeResult:
-        ctw = CountdownTimerWidget()
         yield Container(
-            ctw,
-            CountdownTimerWidget(),
+            CountdownTimerContainer(),
+            CountdownTimerContainer(),
             id="timers",
         )
 
--- pymodoro/widgets/timer.py
+++ pymodoro/widgets/timer.py
@@ -15,48 +15,48 @@ from rich.segment import Segment
 from textual.reactive import reactive
 from textual.widgets import Button, Header, Footer, Static
 
-from threading import RLock
 
-from pymodoro.utils import StateManagement, classproperty
+class Period:
+    def __init__(self):
+        self._start = self._end = 0.0
+
+    def start(self):
+        self._start = self._end = monotonic()
+
+    def stop(self):
+        start = self._start
+        self._start = self._end = 0.0
+        return monotonic() - start
+
+    @property
+    def elapsed(self):
+        if self._start:  # is it running?
+            self._end = monotonic()
+        return self._end - self._start
 
 
-class CountdownTimer(Static, can_focus=True):
-    CSS_PATH = "css/timer.css"
+class CountdownTimer:
+    """manage the logic of a countdown timer"""
 
-    # @classproperty
-    # def state_attrs(cls):
-    #     return 'initial_seconds', '_active', '_elapsed', '_mark_time'
-
-    def __init__(self, initial_seconds=25 * 60, *, id=None):
-        super().__init__(id=id)
+    def __init__(self, initial_seconds=25 * 60):
         self.initial_seconds = initial_seconds
         self._active = False
         self._elapsed = 0.0
-        self._mark_time = 0.0
-
-    def on_mount(self):
-        self._refresh_timer = self.set_interval(1 / 60, self._update, pause=True)
-        self._update()
+        self.period = Period()
 
     def start(self):
         if self._active:
             return
 
         self._active = True
-        self._mark_time = monotonic()
-        self._refresh_timer.resume()
+        self.period.start()
 
     def stop(self):
         if not self._active:
             return
 
-        self._refresh_timer.pause()
+        self._elapsed += self.period.stop()
         self._active = False
-        self._elapsed += self._since_last_mark
-        self._update()
-
-    def _update(self):
-        self.update(self)
 
     @property
     def remaining(self) -> float:
@@ -64,14 +64,34 @@ class CountdownTimer(Static, can_focus=True):
 
     @property
     def total_elapsed(self):
-        current = self._since_last_mark if self._active else 0.0
-        return self._elapsed + current
+        return self._elapsed + self.period.elapsed
 
-    @property
-    def _since_last_mark(self):
-        return monotonic() - self._mark_time
+
+class CountdownTimerWidget(Static, can_focus=True):
+    """wrap a CountdownTimer in a widget"""
+
+    CSS_PATH = "css/timer.css"
+
+    def __init__(self, *, countdown_timer: CountdownTimer, id=None):
+        super().__init__(id=id)
+        self.ct = countdown_timer
+
+    def on_mount(self):
+        self._refresh_timer = self.set_interval(1 / 60, self._update, pause=True)
+        self._update()
+
+    def start(self):
+        self.ct.start()
+        self._refresh_timer.resume()
+
+    def stop(self):
+        self._refresh_timer.pause()
+        self.ct.stop()
+        self._update()
+
+    def _update(self):
+        self.update(self)
 
     def __rich_console__(self, *_):
-        self.log(f"{self._active}, {self.total_elapsed=}")
-        minutes, seconds = divmod(self.remaining, 60)
+        minutes, seconds = divmod(self.ct.remaining, 60)
         yield f"{minutes:02.0f}:{seconds:05.2f}"
